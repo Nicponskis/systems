b60dfd23f9d7b20f94cf9051c6c73bfef7cb5bc1
Dave Nicponski <dave.nicponski@gmail.com>
2023-11-10 05:36:20 -0500

    WIP: disable changedetection

--- hosts/nixos/arm-builder-west/default.nix
+++ hosts/nixos/arm-builder-west/default.nix
@@ -247,7 +247,7 @@ in {
 
   # Watch webpages for changes
   services.changedetection-io = {
-    enable = true;
+    # enable = true;
 
     baseURL = "https://sitechanges.dave.nicponski.dev:${toString (portForwarded changedetection-io-port)}/";
     behindProxy = false;
